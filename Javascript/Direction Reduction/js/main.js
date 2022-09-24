ce73dc07a1ec5359a2ebc1aa11e1b22b6b49ea12
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2022-09-24 11:21:56 -0700

    Update files

--- Javascript/Direction Reduction/js/main.js
+++ Javascript/Direction Reduction/js/main.js
@@ -1,14 +1,18 @@
-//Create a function that will take an array of strings and return the array of strings with a needless direction removed.
+// Create a function that will take an array of strings and return the array of strings with a needless direction removed
+// Assigning opposites to directions and its opposites
+// Assigning results as an array
+// Using forEach method to call one array of the element individually and shorten the loop
+// The pop method will be used to remove the last element in the array
+// The push method will be used to add one more element in the array
+// Return the oppoiste direction in the array
 
 function dirReduc(arr) {
     let opposites = {'NORTH' : 'SOUTH', 'EAST' : 'WEST', 'SOUTH' : 'NORTH', 'WEST' : 'EAST'};
     let result = [];
 
-    //Use a forEach to make it shorten the loop.
     arr.forEach(item => {
         if(result.length) {
 
-            //Use a pop command to remove the last element in the array
             let previousItem = result.pop()
             if(previousItem !== opposites[item]) {
                 result.push(previousItem)
--- Javascript/The Hashtage Generator/js/main.js
+++ Javascript/The Hashtage Generator/js/main.js
@@ -1,21 +1,31 @@
-//Create a function to generate a hashtag generator.
+// Create a function to generate a hashtag generator
+// Assigning newHastTag as a string
+// using split method will the string into bits of strings
+// The filter method to filter down the string to pass a test by the provided condition in the function
+// Assigning hashTag to #
+// Conditionals will be used to allow filtering for the hashtag
+// Will make a condition where the index is 0 on the string without a hashtag to be false
+// Assigning results to result and combine it with newHashTag
+// Using the map method to create a new element in the array by using word as part of the method
+// Assigning capitalWord to word using the charAt method, toUpperCase method, and slice method
+// The charAt method will return a new string
+// The toUppercase method allows each beginning of the string to be capitalized
+// The slice method will be used to start the string at an index
+// Created another conditional if the result is longer then 140 character to return false otherwise return true
 
 function generateHashTag(str) {
     let newHashTag = str.split(' ').filter(char => char !== "");
     let hashTag = "#";
 
-    //Conditional to exclude any spacings using the split command and filter command.
     if(newHashTag.length === 0) {
         return false;
     }
 
-    //Created and and let hastTag be # while using toUpperCase to start with capital letters.
     result = result + newHashTag.map(word => {
         let capitalWord = word.charAt(0) .toUpperCase() + word.slice(1);
         return capitalWord;
     }) .join('');
 
-    //Conditional for characters/symbols over 140.
     if(hashTag.length > 140) {
         return false;
     }else {
